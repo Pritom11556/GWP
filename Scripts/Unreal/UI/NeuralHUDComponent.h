5bf428f279398969187c375ab4b928d436510472
Pritom11556 <Pritomp60@gmail.com>
2025-05-09 16:30:06 +0600

    Delete Scripts/Unreal directory

--- Scripts/Unreal/UI/NeuralHUDComponent.h
+++
@@ -1,8 +0,0 @@
-UCLASS()
-class NEURALUI_API UNeuralHUDComponent : public UActorComponent
-{
-    GENERATED_BODY()
-    
-    UFUNCTION(BlueprintCallable)
-    void UpdateContextualActions(FVector2D TouchDelta);
-};
